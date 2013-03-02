4e2eb9b4e5c1c32cd6993a62bde371c19254f4ac
Li Xuanji <xuanji@gmail.com>
2013-03-02 10:41:03 +0800

    refactor general.c

--- interact.js
+++ interact.js
@@ -6,12 +6,7 @@ cmake ..
 cmake --build .
 ~/emscripten/emcc -s LINKABLE=1 -o libgit.html libgit2.so
 
-//example
+//bindings
+
 var ls = Module.cwrap("ls", 'number', ['string']);
 var general = Module.cwrap("general", 'number', []);
-
-signature_new(1, 'Xuanji Li', 'xuanji@gmail.com', 100, 0)
-gri(10001, "gitteh", 0)
-touch("gitteh/readme")
-repository_index(20001, 10001)
-index_add_bypath(index, "readme")
