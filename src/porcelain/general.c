9cfafaf766dc75c943eb627323d8c71eaadb0300
Li Xuanji <xuanji@gmail.com>
2013-03-31 00:20:20 +0800

    move custom code to porcelain directory

--- src/porcelain/general.c
+++ src/porcelain/general.c
@@ -5,25 +5,38 @@
 
 git_repository* repo;
 
-
-int stage_and_commit(char* filename, char* message) { //only works for a single file
-
-  git_oid tree_oid, commit_oid;
-  git_tree *tree;
-  git_signature *signature;
-  git_index *index;
+int stage(char* filename) {
+  git_index* index;
 
   git_repository_index(&index, repo);
   git_index_add_bypath(index, filename);
   git_index_write(index);
-
-  git_index_write_tree(&tree_oid, index);
+  
   git_index_free(index);
-    
+  return 0;
+}
+
+int commit(char* message) {
+
+  git_index *index;
+  git_oid tree_oid;
+  git_tree *tree;
+  
+  git_oid head_oid, commit_oid;
+  git_commit* head_commit;
+  git_signature *signature;
+  int head_missing;
+
+  git_repository_index(&index, repo);
+  git_index_write_tree(&tree_oid, index);    
   git_tree_lookup(&tree, repo, &tree_oid);
   
   git_signature_new(&signature, "Li Xuanji", "xuanji@gmail.com", 1323847743, 60);
-  git_commit_create_v(
+  
+  head_missing = git_reference_name_to_id(&head_oid, repo, "HEAD");
+  
+  if (head_missing) {
+    git_commit_create_v(
     &commit_oid,
     repo,
     "HEAD",
@@ -33,81 +46,42 @@ int stage_and_commit(char* filename, char* message) { //only works for a single 
     message,
     tree,
     0);
+  } else {
+    git_commit_lookup(&head_commit, repo, &head_oid);
+    git_commit_create_v(
+      &commit_oid,
+      repo,
+      "HEAD",
+      signature,
+      signature,
+      NULL,
+      message,
+      tree,
+      1,
+      head_commit); 
+  }
+
+  //git_index_clear(index);
+  git_index_write(index);
 
   git_tree_free(tree);
+  git_index_free(index);
   git_signature_free(signature);
 
   return 0;
-}
-
-int listrefs() {
-  git_strarray ref_list;
-  git_reference_list(&ref_list, repo, GIT_REF_LISTALL);
-
-  const char *refname;
-  git_reference *ref;
-  
-  char out[41];
-  out[40] = '\0';
-  
-  int i;
-
-  for (i = 0; i < ref_list.count; ++i) {
-    refname = ref_list.strings[i];
-    git_reference_lookup(&ref, repo, refname);
-
-    switch (git_reference_type(ref)) {
-    case GIT_REF_OID:
-      git_oid_fmt(out, git_reference_target(ref));
-      printf("%s [%s]\n", refname, out);
-      break;
-    case GIT_REF_SYMBOLIC:
-      printf("%s => %s\n", refname, git_reference_symbolic_target(ref));
-      break;
-    default:
-      fprintf(stderr, "Unexpected reference type\n");
-      exit(1);
-    }
-  }
-
-  git_strarray_free(&ref_list);
-}
-
-int revwalk_from_head() {
-  git_revwalk *walk;
-  git_commit *wcommit;
-  git_oid oid;
-
-  git_revwalk_new(&walk, repo);
-  
-  char out[41];
-  out[40] = '\0';
-
-  git_revwalk_sorting(walk, GIT_SORT_TOPOLOGICAL);
-
-  git_revwalk_push_head(walk);
-  while ((git_revwalk_next(&oid, walk)) == 0) {
-    git_commit_lookup(&wcommit, repo, &oid);
-    git_oid_fmt(out, &oid);
-    printf("%s\n", out);
-    git_commit_free(wcommit);
-  }
-  git_revwalk_free(walk);
 }
 
 int general() {
   
   git_repository_init(&repo, "zit", 0);
 
-  touch("zit/readme", "Zit \n --- git for zombieis");
-  stage_and_commit("readme", "Initial commit\n\0");
+  touch("zit/readme", "Zit \n --- git for zombieis\n");
+  stage("readme");
+  commit("Initial commit\n");
 
-  touch("zit/TODO", "- Make project logo \n- Watch Friends");
-  stage_and_commit("TODO", "TODO\n\0");
-
-  revwalk_from_head();
-  
-  listrefs();
+  touch("zit/TODO", "- Make project logo \n- Watch Friends\n");
+  stage("TODO");
+  commit("todo\n");
   
   return 0;
 }
