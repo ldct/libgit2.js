987d009c0700458d014b9a6a665706f1bffd998e
Li Xuanji <xuanji@gmail.com>
2013-03-31 11:47:08 +0800

    add showindex

--- interact.js
+++ interact.js
@@ -5,8 +5,3 @@ cd build
 cmake ..
 cmake --build .
 ~/emscripten/emcc -s LINKABLE=1 -o libgit.js libgit2.so
-
-//bindings
-
-var ls = Module.cwrap("ls", 'number', ['string']);
-var general = Module.cwrap("general", 'number', []);
