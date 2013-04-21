226a8992d83c5768cab20e3758564af9c38783d9
Li Xuanji <xuanji@gmail.com>
2013-04-21 17:39:40 +0800

    import the 3ource graph drawer

--- src/porcelain/revwalk.c
+++ src/porcelain/revwalk.c
@@ -6,17 +6,28 @@ int revwalk_from_head(git_repository* repo) {
   git_commit *w_commit;
   git_oid w_oid;
   
-  char out[41];
-  out[40] = '\0';
+  char w_oid_str[41];
+  w_oid_str[40] = '\0';
+
+  char p_oid_str[41];
+  p_oid_str[40] = '\0';
 
   git_revwalk_new(&walk, repo);
   git_revwalk_sorting(walk, GIT_SORT_TOPOLOGICAL);
   
   git_revwalk_push_head(walk);
   while ((git_revwalk_next(&w_oid, walk)) == 0) {
-    git_oid_fmt(out, &w_oid);
+    git_oid_fmt(w_oid_str, &w_oid);
     git_commit_lookup(&w_commit, repo, &w_oid);
-    printf("%s%s%s", out, git_commit_message(w_commit), out);
+    int parent_count = git_commit_parentcount(w_commit);
+    printf("%s", w_oid_str);
+    printf("%i ", parent_count);
+    for (int i = 0; i < parent_count; i++) {
+      git_oid_fmt(p_oid_str, git_commit_parent_id(w_commit, i));
+      printf("%s ", p_oid_str);
+    }
+    printf("%s", git_commit_message(w_commit));
+    printf("%s", w_oid_str);
     git_commit_free(w_commit);
   }
   git_revwalk_free(walk);
