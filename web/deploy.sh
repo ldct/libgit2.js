4aa4174f0bccc70711625606914055ecf4a53eb1
Li Xuanji <xuanji@gmail.com>
2013-05-16 20:38:44 +0800

    fix typo

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,4 +1,4 @@
 rm *~
 cp -avr . ../../appspot-grading/libgit2-js
-rm ../../appspot-grading/libgit2-js/libgit.compressed.js
-cp ../build/libgit.compressed.js ../../appspot-grading/libgit2-js
+rm ../../appspot-grading/libgit2-js/libgit.js.compress
+cp ../build/libgit.js.compress ../../appspot-grading/libgit2-js
