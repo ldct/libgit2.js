7bac272669c692de19be0037bd2f1932db9a48bb
Li Xuanji <xuanji@gmail.com>
2013-05-23 14:11:06 +0800

    make terminal and explorer draggable, make terminal transparent, make svg fill whole screen

---
+++ web/drag.js
@@ -0,0 +1,39 @@
+var drag_target = null;
+
+function drag_start(event) {
+    console.log("dragstart fired with ", this.id);
+    drag_target = this.id;
+    var style = window.getComputedStyle(event.target, null);
+    event.dataTransfer.setData("text/plain",
+    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+} 
+function drag_over(event) { 
+    event.preventDefault(); 
+    return false; 
+}
+function clamp(n) {
+  return n > 0 ? n : 0;
+}
+function make_drop(target) {
+    return function() {
+        if (target != drag_target) {
+            return false;
+        }
+        var offset = event.dataTransfer.getData("text/plain").split(',');
+        var dm = document.getElementById(target);
+        dm.style.left = clamp(event.clientX + parseInt(offset[0],10)) + 'px';
+        dm.style.top = clamp(event.clientY + parseInt(offset[1],10)) + 'px';
+        event.preventDefault();
+        return false;
+    }    
+} 
+
+function make_draggable(target) {
+    var dm = document.getElementById(target); 
+    dm.addEventListener('dragstart',drag_start,false); 
+    document.body.addEventListener('dragover',drag_over,false); 
+    document.body.addEventListener('drop',make_drop(target),false);
+}
+
+make_draggable("term_drag");
+make_draggable("explorer_drag");
--- web/libgit-pre.js
+++ web/libgit-pre.js
@@ -1,44 +1,84 @@
-      var Module = {
-        std_out: [],
-        preRun: [],
-        postRun: [function() {      
-          Module.ccall("general", 'number', [] ,[]);
-          make_branch("twig");
-          checkout("twig");
-          commit("twigglet");
-          checkout("master");
-          commit("hi");
-          show_dir(ls());
-          $("#text_editor").val("This is a text editor - click on a filename above to edit the file!\nThe bold entries are directories - click to navigate")
-          updateGraph();
-        }],
-        print: function(text) {
-          Module.std_out.push(text);
-        },
-        printErr: function(text) {
-          console.log("error: ", text);
-        },
-        setStatus: function(text) {
-          if (Module.setStatus.interval) clearInterval(Module.setStatus.interval);
-          var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
-          var statusElement = document.getElementById('status');
-          var progressElement = document.getElementById('progress');
-          if (m) {
-            text = m[1];
-            progressElement.value = parseInt(m[2])*100;
-            progressElement.max = parseInt(m[4])*100;
-            progressElement.hidden = false;
-          } else {
-            progressElement.value = null;
-            progressElement.max = null;
-            progressElement.hidden = true;
-          }
-          statusElement.innerHTML = text;
-        },
-        totalDependencies: 0,
-        monitorRunDependencies: function(left) {
-          this.totalDependencies = Math.max(this.totalDependencies, left);
-          Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
-        }
-      };
-      Module.setStatus('Downloading...(~2 MB)');
+var Module = {
+  std_out: [],
+  preRun: [],
+  postRun: [function() {      
+    Module.ccall("general", 'number', [] ,[]);
+    make_branch("twig");
+    checkout("twig");
+    commit("twigglet");
+    checkout("master");
+    commit("hi");
+    show_dir(ls());
+    $("#text_editor").val("This is a text editor - click on a filename above to edit the file!\nThe bold entries are directories - click to navigate")
+    updateGraph();
+  }],
+  print: function(text) {
+    Module.std_out.push(text);
+  },
+  printErr: function(text) {
+    console.log("error: ", text);
+  },
+  setStatus: function(text) {
+    if (Module.setStatus.interval) clearInterval(Module.setStatus.interval);
+    var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+    var statusElement = document.getElementById('status');
+    var progressElement = document.getElementById('progress');
+    if (m) {
+      text = m[1];
+      progressElement.value = parseInt(m[2])*100;
+      progressElement.max = parseInt(m[4])*100;
+      progressElement.hidden = false;
+    } else {
+      progressElement.value = null;
+      progressElement.max = null;
+      progressElement.hidden = true;
+    }
+    statusElement.innerHTML = text;
+  },
+  totalDependencies: 0,
+  monitorRunDependencies: function(left) {
+    this.totalDependencies = Math.max(this.totalDependencies, left);
+    Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
+  }
+};
+Module.setStatus('Downloading...(~2 MB)');
+
+var decompressWorker = new Worker('decompress.js');
+var decompressCallbacks = [];
+var decompressions = 0;
+Module["decompress"] = function(data, callback) {
+  var id = decompressCallbacks.length;
+  decompressCallbacks.push(callback);
+  decompressWorker.postMessage({ data: data, id: id });
+  if (Module['setStatus']) {
+    decompressions++;
+    Module['setStatus']('Decompressing... (chrome users: lzma decompression uses lots of memory, killing the tab. closed other processes that use too much memory)');
+  }
+};
+decompressWorker.onmessage = function(event) {
+  decompressCallbacks[event.data.id](event.data.data);
+  decompressCallbacks[event.data.id] = null;
+  if (Module['setStatus']) {
+    decompressions--;
+    if (decompressions == 0) {
+      Module['setStatus']('');
+      FS.checkStreams = function() {return;};//TODO: file bug report against too many FS.streams
+    }
+  }
+};
+var compiledCodeXHR = new XMLHttpRequest();
+compiledCodeXHR.open('GET', 'libgit.js.compress', true);
+compiledCodeXHR.responseType = 'arraybuffer';
+compiledCodeXHR.onload = function() {
+  var arrayBuffer = compiledCodeXHR.response;
+  if (!arrayBuffer) throw('Loading compressed code failed.');
+  var byteArray = new Uint8Array(arrayBuffer);
+  Module.decompress(byteArray, function(decompressed) {
+    var source = Array.prototype.slice.apply(decompressed).map(function(x) { return String.fromCharCode(x) }).join(''); // createObjectURL instead?
+    var scriptTag = document.createElement('script');
+    scriptTag.setAttribute('type', 'text/javascript');
+    scriptTag.innerHTML = source;
+    document.body.appendChild(scriptTag);
+  });
+};
+compiledCodeXHR.send(null);
